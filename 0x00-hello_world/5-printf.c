af2e5d4936fa951a5a099e7807c9a2568937b690
mugonaan-0097 <mugonaan@gmail.com>
2022-09-09 01:23:23 -0700

    printsit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,9 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (0)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
 }
