06584f4a4eef7bdde0bcf29b181ae1d7e4421eba
mugonaan-0097 <mugonaan@gmail.com>
2022-09-13 15:57:55 -0700

    putchar dem

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main - print "_putchar"
- * Return: Always 0.
+ * main - print the string "_putchar" from a character array.
+ * Return: 0 on success.
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	char c[] = "_putchar";
+	int i;
 
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
