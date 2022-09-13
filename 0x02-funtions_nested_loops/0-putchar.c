29a0d898bc5a0e479729c9b13ec5ff78f86aadf6
mugonaan-0097 <mugonaan@gmail.com>
2022-09-13 14:29:06 -0700

    putchar

---
+++ 0x02-funtions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - print the string "_putchar"
+ * Return: Always 0 on success.
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
