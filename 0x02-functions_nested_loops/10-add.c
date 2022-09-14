1c8db8be6976948a28460e39fcb526124077f819
mugonaan-0097 <mugonaan@gmail.com>
2022-09-14 13:14:34 -0700

    headache

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * add - adds two numbers together.
+ * add - add two numbers together.
  * @a: The first number to be added.
  * @b: The second number to be added.
  * Return: the sum of two numbers.
