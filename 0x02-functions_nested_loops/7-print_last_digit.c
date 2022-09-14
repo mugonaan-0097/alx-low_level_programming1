0b6b8c885203d440ad25f641153d34ca7ec76caf
mugonaan-0097 <mugonaan@gmail.com>
2022-09-14 07:32:45 -0700

    kilo

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_last_digit - print the last digit of a number.
+ * print_last_digit - prints the last digit of a number.
  * @n: digit to find the last place of.
  * Return: The last digit
  */
